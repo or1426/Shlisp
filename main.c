931dfef97268b6af6f6ea8b01d16f7c691524d46
or1426 <or1426@my.bristol.ac.uk>
2013-01-31 01:01:01 +0000

    Not yet working

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ struct lisp_object  new_string_object(char *);
 struct lisp_object  new_bool_object(int);
 struct lisp_object  new_char_object(char);
 struct lisp_object  new_proc_object(char *,struct lisp_object* (struct lisp_object *));
-struct lisp_object  new_pair_object(struct lisp_object*,struct lisp_object*);
+struct lisp_object  new_pair_object(struct lisp_object *,struct lisp_object*);
 
 struct lisp_object  add      (struct lisp_object a,struct lisp_object b){return new_num_object(a.numD + b.numD);}
 struct lisp_object  subtract (struct lisp_object a,struct lisp_object b){return new_num_object(a.numD - b.numD);}
@@ -54,6 +54,11 @@ struct lisp_object car (struct lisp_object);
 struct lisp_object cdr (struct lisp_object);
 
 char *removeWhitesapce(char *);  // I am a twat.
+
+struct lisp_object listeriseString(char *);
+
+int * bracketMatcher(char *);
+int * bracketMap(char *);
 
 void lisp_write(struct lisp_object);
 
@@ -89,11 +94,12 @@ int main()
     strcpy(opArray[i-1].opName, "/");
     opArray[i-1].opPointer = &divide;
     */
-    printf("++Because this is currently shit all operations have to be done inside a list++\n");
+    //printf("++Because this is currently all operations have to be done inside a list++\n");
     printf("++++++++++++++++++++Various other things are also a bit shit+++++++++++++++++++\n\n");
     do
     {
         printf(">");
+        //struct lisp_object m = lisp_eval(lisp_read());
         lisp_write((lisp_eval(lisp_read())));
     }
     while(1);
@@ -103,6 +109,7 @@ struct lisp_object lisp_eval(struct lisp_object objEval)
 {
     switch(objEval.objectType){
         case T_string :
+            //printf("Fuck\n");
             return objEval;
         case T_num :
             return objEval;
@@ -112,11 +119,12 @@ struct lisp_object lisp_eval(struct lisp_object objEval)
             return objEval;
         case T_procedure:
             return objEval;
-        case T_pair :
-            if(car(objEval).objectType == T_procedure){
-            }else{
-                return objEval;
-            }
+        case T_pair:
+        printf("Ok....");
+           // if(car(objEval).objectType == T_procedure){
+            //}else{
+            return objEval;
+            //}
     }
         //return (*(objEval[0].proc)).opPointer(lisp_eval(&objEval[1])) ;
 }
@@ -146,8 +154,6 @@ struct lisp_object lisp_read(){
     while((c =getchar()) != EOF)
     {
         stringBuffer = realloc(stringBuffer, i+ 1);
-        stringBuffer[i]=c;
-
 
         if(c=='(')
         {
@@ -163,32 +169,123 @@ struct lisp_object lisp_read(){
             stringBuffer[++i]='\0';
             break;
         }
-
+        stringBuffer[i]=c;
         ++i;
     }
 
-    /* Doing it twice just to make sure some really rare bugs don't happen */
+    /* Doing it twice just to make sure some really rare bugs don't happen (also for the shear goddamn hell of it)*/
     stringBuffer = removeWhitesapce(stringBuffer);
     stringBuffer = removeWhitesapce(stringBuffer);
 
-    return new_string_object(stringBuffer);
+    listeriseString(stringBuffer);
+    return new_num_object(0);
 }
+struct lisp_object listeriseString(char * current){
+
+    int * bM = malloc(sizeof(int) * strlen(current));
+    bM = bracketMatcher(current);
+    int *unProtSpaceLoc = NULL;
+    int i,d = 0;
+    struct lisp_object * stringArray;
+    //printf("%d\n",strlen((*inp.pairD.current).stringD));
+    if(current[0] =='(' && bM[0] == (strlen(current) - 1)){  //Hopefully this is equiv to saying whole statement is in a bracket!
+       current[strlen(current) - 1] = '\0'; //remove last bracket
+       ++current; //move pointer to get rid of first bracket
+    }
+    printf("%s\n",current);
+
+    bM = bracketMap(current);
+    printf("%s\n",current);
+    for(i=0;i<strlen(current);++i){ //loop to locate the unprotected spaces (parenthesis protect spaces)
+        if(bM[i]==0 && current[i] == ' '){
+            ++d;
+            unProtSpaceLoc = realloc(unProtSpaceLoc,d*sizeof(int)); //make space for new location
+            if(d != 1){
+                unProtSpaceLoc[d- 1] = i;
+            }else{
+                printf("Lo\n");
+                unProtSpaceLoc[d- 1] = i;
+            }
+            printf("%d ",unProtSpaceLoc[d- 1]);
+        }
+    }
+    stringArray = malloc(d*sizeof(stringArray));
+    printf("\n\n");
+    for(i= d-1;i>=0;--i){
+        if(i != 0){
+            unProtSpaceLoc[i] = unProtSpaceLoc[i] - unProtSpaceLoc[i-1]; //turn it into the difference (makes next part easier to write
+        }
+    }
+
+    for(i= d-1;i>=0;--i){
+        if(i != 0){
+
+        }
+    }
+
+
+    printf("\n");
+}
+
+int * bracketMatcher(char * inp){
+   int * bm = malloc(sizeof(int) *strlen(inp) );
+   bm = bracketMap(inp);
+   int * matchMap = malloc(sizeof(int) *strlen(inp) );
+   int i = 0,j = 0;
+   for(i = 0;i<strlen(inp);++i){
+       j = 1;
+       if(inp[i] == '('){
+          while(bm[i + j] != bm[i]){
+          ++j;
+          }
+          matchMap[i] = i+j;
+        }else{
+            matchMap[i] = -1;
+        }
+        printf("%d",matchMap[i]);
+
+    }
+    printf("\n");
+   return matchMap;
+}
+
+int * bracketMap(char * inp){
+int i = 0;
+int *bracketMap = malloc(sizeof(int) *strlen(inp) );
+int depth = 0;
+    for(i = 0;i<strlen(inp) ;++i){
+        if(inp[i] == '('){
+           bracketMap[i] = depth;
+           ++depth;
+        }else if(inp[i] == ')'){
+           --depth;
+            bracketMap[i] = depth;
+        }else{
+            bracketMap[i] = depth;
+        }
+    printf("%d",bracketMap[i]);
+
+    }
+    printf("\n");
+    return bracketMap;
+}
+
 
 char *removeWhitesapce(char *stringBuffer){
     int i;
     int j;
-    int length=strlen(stringBuffer);
-
-    for(i=0; i<length; ++i)
-    {
-
-        if(stringBuffer[0]==' ')
+    int length=strlen(stringBuffer)+1;
+if(stringBuffer[0]==' ')
         {
             for(j=0; j<length; ++j)
                 stringBuffer[j]=stringBuffer[1+j];
 
             i=0;
         }
+    for(i=0; i<length; ++i)
+    {
+
+
 
         if(stringBuffer[i]=='\n')
         {
@@ -228,19 +325,15 @@ char *removeWhitesapce(char *stringBuffer){
 
                 i=0;
             }
-
-
-
-
-
     }
 
     return stringBuffer;
 }
 
 void lisp_write(struct lisp_object objOut){
-
- switch(objOut.objectType){
+    //printf("Damn\n");
+    int h = objOut.objectType;
+ switch(h){
         case T_string :
             printf("%s\n", objOut.stringD);
             break;
@@ -257,9 +350,11 @@ void lisp_write(struct lisp_object objOut){
             printf("%s\n", (*objOut.proc).opName );
             break;
         case T_pair :
+            printf("You crazy bastard!\n");
             break;
        }
 }
+
 
 struct lisp_object new_num_object(double d){
     struct lisp_object r;
@@ -279,9 +374,11 @@ struct lisp_object new_string_object(char *d){
     r.charD = ' ';
     r.numD = 0;
     r.proc = NULL;
-    r.stringD = malloc(sizeof(d));
-    r.pairD = new_cons(NULL,NULL);
+    r.stringD = malloc(strlen(d) + 1);
+
     strcpy(r.stringD,d);
+
+    r.pairD = new_cons(NULL,NULL);
 
     return r;
 }
